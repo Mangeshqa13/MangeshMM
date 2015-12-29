94da423a6f0e07cc9f24e132bdce64565b07e46b
mangesh <mangeshqa13@gmail.com>
2015-12-29 12:57:01 +0530

    commit

---
+++ src/test/java/MangMaven/GoogleLaunch.java
@@ -0,0 +1,14 @@
+package MangMaven;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleLaunch {
+
+	public static void main(String[] args) {
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://google.co.in");
+
+	}
+
+}
